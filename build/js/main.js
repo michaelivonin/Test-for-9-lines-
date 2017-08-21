bf7d5fc4c72688a4644c51fe158b3fa14cd5d2aa
Michael Ivonin <Michael@drummer.ru>
2017-08-21 12:35:54 +0600

    Almost done

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,23 @@
 $(function() {
+  function countIt() {
+    var i = 0,
+        timerId = setInterval(countTo100,10);
 
-    // Если в проекте используются встроенные js-плагины от Foundation, разкомментировать
-    // $(document).foundation();
+    function countTo100(){
+      $('.js-level__counter').text(++i + '%');
+      if (i === 100) {
+        clearInterval(timerId);
+        timerId = setInterval(countTo40,15);
+      }
+    }
 
+    function countTo40(){
+      $('.js-level__counter').text(--i + '%');
+      if (i === 40) {
+        clearInterval(timerId);
+      }
+    }
+  }
+
+  setTimeout(countIt, 2000);
 });
